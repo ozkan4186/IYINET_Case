5ae3f6509334ceeedad21666ba3fb0bc39dadced
Özkan Demir <ozkanndemir2009@gmail.com>
2024-01-03 23:25:41 +0300

    Link added transition done

--- src/components/Navbar/page.jsx
+++ src/components/Navbar/page.jsx
@@ -5,6 +5,7 @@ import { FaSackXmark } from "react-icons/fa6";
 import { GiCellarBarrels } from "react-icons/gi";
 import { GiBarrel } from "react-icons/gi";
 import { LiaShirtsinbulk } from "react-icons/lia";
+import Link from 'next/link';
 const Navbar = () => {
   return (
     <div className="container mx-auto my-9 sm:w-full md:w-3/4 lg:w-[56rem] ">
@@ -15,34 +16,34 @@ const Navbar = () => {
         </h1>
       </div>
       <div className="flex flex-wrap cursor-pointer gap-2 ">
-        <div className="flex-col gap-2 p-10 shadow-lg bg-white hover:bg-blue-600 rounded-lg ">
+        <Link href={"/box"} className="flex-col gap-2 p-10 shadow-lg bg-white hover:bg-blue-600 rounded-lg ">
           <TbBoxSeam />
           <p>box</p>
-        </div>
-        <div className="flex-col gap-2 p-8 shadow-lg  bg-white hover:bg-blue-600 rounded-lg">
+        </Link>
+        <Link href={"/bigbags"} className="flex-col gap-2 p-8 shadow-lg  bg-white hover:bg-blue-600 rounded-lg">
           <PiToolboxLight />
           <p>bigbags</p>
-        </div>
-        <div className="flex-col gap-2 p-8 shadow-lg  bg-white hover:bg-blue-600 rounded-lg ">
+        </Link>
+        <Link href={"/sacks"} className="flex-col gap-2 p-8 shadow-lg  bg-white hover:bg-blue-600 rounded-lg ">
           <FaSackXmark />
           <p>sacks</p>
-        </div>
-        <div className="flex-col gap-2 p-8  shadow-lg  bg-white hover:bg-blue-600 rounded-lg">
+        </Link>
+        <Link href={"/barrels"} className="flex-col gap-2 p-8  shadow-lg  bg-white hover:bg-blue-600 rounded-lg">
           <GiBarrel />
           <p>barrels</p>
-        </div>
+        </Link>
         <div className="flex-col align-middle justify-center items-center gap-2 p-10  shadow-lg  bg-white hover:bg-blue-600 rounded-lg ">
           <GiCellarBarrels />
           <p>roll</p>
         </div>
-        <div className="flex-col gap-2 p-8 shadow-lg bg-orange-100 rounded-lg   hover:bg-blue-600  ">
+        <Link href={"/pipes"} className="flex-col gap-2 p-8 shadow-lg bg-orange-100 rounded-lg   hover:bg-blue-600  ">
           <GiCellarBarrels />
           <p>pipes</p>
-        </div>
-        <div className="flex-col gap-2 p-8 shadow-lg  bg-orange-100 rounded-lg hover:bg-blue-600 ">
+        </Link>
+        <Link href={"/bulk"} className="flex-col gap-2 p-8 shadow-lg  bg-orange-100 rounded-lg hover:bg-blue-600 ">
           <LiaShirtsinbulk />
           <p>bulk</p>
-        </div>
+        </Link>
       </div>
     </div>
   );
