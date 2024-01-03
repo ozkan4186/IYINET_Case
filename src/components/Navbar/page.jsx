40613af19ca647ca628981ce87cd3e739210a0fa
Özkan Demir <ozkanndemir2009@gmail.com>
2024-01-04 02:30:45 +0300

    box component added png done

--- src/components/Navbar/page.jsx
+++ src/components/Navbar/page.jsx
@@ -20,49 +20,49 @@ const Navbar = () => {
           href={"/box"}
           className="flex-col gap-2 p-10 shadow-lg bg-white hover:bg-blue-600 rounded-lg "
         >
-          <TbBoxSeam />
-          <p>box</p>
+          <TbBoxSeam className="m-auto text-5xl text-gray-500 " />
+          <p className="ml-3">box</p>
         </Link>
         <Link
           href={"/bigbags"}
           className="flex-col gap-2 p-8 shadow-lg  bg-white hover:bg-blue-600 rounded-lg"
         >
-          <PiToolboxLight />
+          <PiToolboxLight className="m-auto text-5xl text-gray-500 " />
           <p>bigbags</p>
         </Link>
         <Link
           href={"/sacks"}
           className="flex-col gap-2 p-8 shadow-lg  bg-white hover:bg-blue-600 rounded-lg "
         >
-          <FaSackXmark />
+          <FaSackXmark className="m-auto text-5xl text-gray-500 " />
           <p>sacks</p>
         </Link>
         <Link
           href={"/barrels"}
           className="flex-col gap-2 p-8  shadow-lg  bg-white hover:bg-blue-600 rounded-lg"
         >
-          <GiBarrel />
+          <GiBarrel className="m-auto text-5xl text-gray-500" />
           <p>barrels</p>
         </Link>
         <Link
           href={"/roll"}
           className="flex-col align-middle justify-center items-center gap-2 p-10  shadow-lg  bg-white hover:bg-blue-600 rounded-lg "
         >
-          <GiCellarBarrels />
+          <GiCellarBarrels className="m-auto text-5xl text-gray-500 " />
           <p>roll</p>
         </Link>
         <Link
           href={"/pipes"}
           className="flex-col gap-2 p-8 shadow-lg bg-orange-100 rounded-lg   hover:bg-blue-600  "
         >
-          <GiCellarBarrels />
+          <GiCellarBarrels className="m-auto text-5xl text-gray-500 " />
           <p>pipes</p>
         </Link>
         <Link
           href={"/bulk"}
           className="flex-col gap-2 p-8 shadow-lg  bg-orange-100 rounded-lg hover:bg-blue-600 "
         >
-          <LiaShirtsinbulk />
+          <LiaShirtsinbulk className="m-auto text-5xl text-gray-500 " />
           <p>bulk</p>
         </Link>
       </div>
--- src/components/box/page.jsx
+++ src/components/box/page.jsx
@@ -4,91 +4,118 @@ import img from "../../../public/den.png";
 
 const Box = () => {
   return (
-    <div
-      className="container mx-auto my-9 sm:w-full md:w-3/4 lg:w-[56rem] leading-10 "
-      style={{
-        backgroundColor: "#F6F6F6",
-      }}
-    >
-      <div>
-        <h1>
-          <span className="text-blue-500">1.</span>{" "}
-          <span className="text-blue-900 font-bold ">
-            SELECT CARGO DİMENSİONS
-          </span>
-        </h1>
-      </div>
-      <div className="flex flex-wrap cursor-pointer gap-2 ">
+    <div>
+      <div
+        className="container mx-auto my-9 sm:w-full md:w-3/4 lg:w-[56rem] leading-10 "
+        style={{
+          backgroundColor: "#F6F6F6",
+        }}
+      >
         <div>
-          <Image
-            src={img}
-            width={300}
-            height={900}
-            alt="Picture of the author"
-          />
+          <h1>
+            <span className="text-blue-500">2.</span>{" "}
+            <span className="text-blue-900 font-bold ">
+              SELECT CARGO DİMENSİONS
+            </span>
+          </h1>
         </div>
-        <form action="">
-          <div className="flex gap-3 ">
-            <div>
-              <label className="text-gray-500 mb-5 " htmlFor="">
-                Product Name
-              </label>{" "}
-              <br />
-              <input className="rounded-lg px-5 " type="text" />
+        <div className="flex flex-wrap cursor-pointer gap-2 ">
+          <div>
+            <Image
+              src={img}
+              width={300}
+              height={900}
+              alt="Picture of the author"
+            />
+          </div>
+          <form action="">
+            <div className="flex gap-3 ">
+              <div>
+                <label className="text-gray-500 mb-5 " htmlFor="">
+                  Product Name
+                </label>{" "}
+                <br />
+                <input className="rounded-lg px-5 " type="text" />
+              </div>
+              <div>
+                <label className="text-gray-500" htmlFor="">
+                  Color
+                </label>
+                <br />
+                <input className="rounded-lg" type="text" />
+              </div>
             </div>
+            <div className="flex gap-3 ">
+              <div>
+                <label className="text-gray-500 mb-5 " htmlFor="">
+                  Length
+                </label>{" "}
+                <br />
+                <input className="rounded-lg w-32" type="text" />
+              </div>
+              <div>
+                <label className="text-gray-500" htmlFor="">
+                  Width
+                </label>{" "}
+                <br />
+                <input className="rounded-lg w-32" type="text" />
+              </div>
+              <div>
+                <label className="text-gray-500" htmlFor="">
+                  Height
+                </label>{" "}
+                <br />
+                <input className="rounded-lg w-32" type="text" />
+              </div>
+            </div>
+            <div className="flex gap-3 ">
+              <div>
+                <label className="text-gray-500 mb-5 " htmlFor="">
+                  Length
+                </label>{" "}
+                <br />
+                <input className="rounded-lg w-32" type="text" />
+              </div>
+              <div>
+                <label className="text-gray-500" htmlFor="">
+                  Width
+                </label>{" "}
+                <br />
+                <input className="rounded-lg w-32" type="text" />
+              </div>
+              <div className="flex gap-3 mt-5 ">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold w-44 h-10 mt-5 text-center rounded-3xl">
+                  Prediction
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="container mx-auto flex gap-8 my-9 sm:w-full md:w-3/4 lg:w-[56rem] ">
+        <div>
+          <h1>
+            <span className="text-blue-500">3.</span>
+            <span className="text-blue-900 font-bold ">
+              SPACING SETTINGS ?{" "}
+            </span>
+          </h1>
+          <div className="flex flex-wrap cursor-pointer gap-2 ">
             <div>
-              <label className="text-gray-500" htmlFor="">
-                Color
-              </label>{" "}
-              <br />
-              <input className="rounded-lg" type="text" />
+              <label>
+                <input type="checkbox" checked /> Tilt to Length
+              </label>
             </div>
           </div>
-          <div className="flex gap-3 ">
-            <div>
-              <label className="text-gray-500 mb-5 " htmlFor="">
-                Length
-              </label>{" "}
-              <br />
-              <input className="rounded-lg w-32" type="text" />
-            </div>
-            <div>
-              <label className="text-gray-500" htmlFor="">
-                Width
-              </label>{" "}
-              <br />
-              <input className="rounded-lg w-32" type="text" />
-            </div>
-            <div>
-              <label className="text-gray-500" htmlFor="">
-                Height
-              </label>{" "}
-              <br />
-              <input className="rounded-lg w-32" type="text" />
-            </div>
-          </div>
-          <div className="flex gap-3 ">
-            <div>
-              <label className="text-gray-500 mb-5 " htmlFor="">
-                Length
-              </label>{" "}
-              <br />
-              <input className="rounded-lg w-32" type="text" />
-            </div>
-            <div>
-              <label className="text-gray-500" htmlFor="">
-                Width
-              </label>{" "}
-              <br />
-              <input className="rounded-lg w-32" type="text" />
-            </div>
-            <div className="flex gap-3 mt-5 ">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold w-44 h-10 mt-5 text-center rounded-3xl">
-                Prediction
-              </button>
-            </div>
-          </div>
-        </form>
+        </div>
+
+        <div>
+          <h1>
+            <span className="text-blue-500">4.</span>
+            <span className="text-blue-900 font-bold ">STUFFING SETTINGS</span>
+          </h1>
+        </div>
+        <div className="flex flex-wrap cursor-pointer gap-2 "></div>
       </div>
     </div>
   );
